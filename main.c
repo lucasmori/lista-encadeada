2be6543f1e1c04108bcd41c7d92665192afa0a37
Lucas <lucastecnew@gmail.com>
2019-04-07 21:46:23 -0300

    Menu Atualizado

--- main.c
+++ main.c
@@ -58,41 +58,42 @@ void tela(){
         //Menu Principal
         gotoxy(55,10);printf("MENU");
         gotoxy(45,12);printf("1 - Cadastro de Alunos");
-        gotoxy(45,13);printf("2 - Remover Aluno Específico");
+        gotoxy(45,13);printf("2 - Remover Aluno Especifico");
         gotoxy(45,14);printf("3 - Remover Todos os Alunos");
         gotoxy(45,15);printf("4 - Buscar Aluno");
-        gotoxy(45,16);printf("5 - Exibir Alunos de Forma Ordenada");
-        gotoxy(45,17);printf("6 - Relatório");
-        gotoxy(45,18);printf("7 - Salvar em um Arquivo texto");
-        gotoxy(45,19);printf("8 - Salvar em um Arquivo Binário");
-        gotoxy(45,20);printf("9 - Carregar de um Arquivo Binário");
-        gotoxy(45,21);printf("10 - Exibe a Media do IMC e o Desvio Padrão"); // Opção para Exibir a Media do IMC e o Desvio Padrão
-        gotoxy(45,22);printf("11 - Exibe Maior e menor altura Exibe Maior e menor Massa");// Opção para Exibir os funções do Davi
-        gotoxy(45,23);printf("0 - Sair");
+        gotoxy(45,16);printf("5 - Salvar em um Arquivo texto");
+        gotoxy(45,17);printf("6 - Exibe a Media do IMC e o Desvio Padrao"); // Opção para Exibir a Media do IMC e o Desvio Padrão
+        gotoxy(45,18);printf("7 - Exibe Maior e Menor Altura, Exibe Maior e Menor Massa");// Opção para Exibir os funções do Davi
+        gotoxy(45,19);printf("0 - Sair");
+       // gotoxy(45,16);printf("5 - Exibir Alunos de Forma Ordenada");
+       // gotoxy(45,17);printf("6 - Relatório");
+       // gotoxy(45,19);printf("6 - Salvar em um Arquivo Binário");
+       // gotoxy(45,20);printf("9 - Carregar de um Arquivo Binário");
 
-        gotoxy(45,25);printf("Escolha a opção desejada: ");
-        gotoxy(71,25);scanf("%d",&opcao);
+        gotoxy(45,23);printf("Escolha a opcao desejada: ");
+        gotoxy(71,23);scanf("%d",&opcao);
         fflush(stdin);
         system("cls");
 
         switch(opcao){
 
-            case 0: ;break;
+            case 0: break;
             case 1: inserir_aluno(lista);break;
             case 2: remover_especifico(lista);break;
             case 3: excluir_todos(lista);break;
             case 4: buscar_aluno(lista);break;
-            /*case 5: exibir_alunos(lista);break;
-            case 6: relatorio();break;*/
-            case 7: salvar_alunos(lista);break;
-            case 8: salvar_binario(lista);break;
-            case 9: carregar_alunos(lista);break;
-            case 10: retorna_media_imc(lista); retorna_desvio_padrao(lista);break; // Chamada das funções de Media e desvio do IMC
-            case 11: retorna_maior_altura(lista); retorna_menor_altura(lista); retorna_maior_massa(PTR_LISTA lista); retorna_menor_massa(PTR_LISTA lista);break; // Chamada das funções do Davi e do Samuel
-        }
+            case 5: salvar_alunos(lista);break;
+            case 6: retorna_media_imc(lista); retorna_desvio_padrao(lista);break; // Chamada das funções de Media e desvio do IMC
+            case 7: todos_maior_menor(lista);break; // Chamada das funções do Davi
+         // case 5: exibir_alunos(lista);break;
+         // case 6: relatorio();break;
+         // case 8: salvar_binario(lista);break;
+         // case 9: carregar_alunos(lista);break;
+       }
     }
     system("pause");
  }
+
 
 void salvar_alunos(PTR_LISTA lista) {
     if(salvar(lista) == 0) {
@@ -109,3 +110,4 @@ void carregar_alunos(PTR_LISTA lista) {
         gotoxy(45,25);printf("Alunos recuperados com sucesso!");
     }
 }
+
